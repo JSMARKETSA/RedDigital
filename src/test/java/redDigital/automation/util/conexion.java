9e4263a3ebfdeb9f54e9d8d98e9945824fcdadc5
alexgp94 <alexcgp94@gmail.com>
2020-04-15 21:24:49 -0500

    new code1

--- src/test/java/redDigital/automation/util/conexion.java
+++ src/test/java/redDigital/automation/util/conexion.java
@@ -9,7 +9,7 @@ public class conexion {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             con= DriverManager.getConnection("jdbc:sqlserver://JSMARKET\\JSMSPROD;databaseName=RedDigital","ROBOTJSMS","R0b0t_JSMS");
-            System.out.println("conexion hecha");
+            System.out.println("conexion hecha sql");
 
         } catch (Exception e) {
             System.err.println("Error:" +e);
