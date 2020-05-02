c50e85ed340b26bb9df3e5f4cb40d3ea84174d14
alexgp94 <alexcgp94@gmail.com>
2020-05-01 20:05:47 -0500

    fix users

--- src/test/java/redDigital/automation/pages/ModalLogin.java
+++ src/test/java/redDigital/automation/pages/ModalLogin.java
@@ -64,27 +64,27 @@ public class ModalLogin extends PageObject {
         public Connection con;
 
         public conexion() {
-//            try {
-//                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//                con= DriverManager.getConnection("jdbc:sqlserver://JSMARKET\\JSMSPROD;databaseName=RedDigital","ROBOTJSMS","R0b0t_JSMS");
-//                System.out.println("conexion hecha sqlServer");
-//            } catch (Exception e) {
-//                System.err.println("Error:" +e);
-//            }
-
             try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/redDigital", "root", "Passwod$1");
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                con= DriverManager.getConnection("jdbc:sqlserver://JSMARKET\\JSMSPROD;databaseName=RedDigital","ROBOTJSMS","R0b0t_JSMS");
+                System.out.println("conexion hecha sqlServer");
             } catch (Exception e) {
-                System.err.println("Error:" + e);
+                System.err.println("Error:" +e);
             }
+
+//            try {
+//                Class.forName("com.mysql.cj.jdbc.Driver");
+//                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/redDigital", "root", "Passwod$1");
+//            } catch (Exception e) {
+//                System.err.println("Error:" + e);
+//            }
 
         }
     }
 
     private WebDriver getAllWebDriver() {
         WebDriverFacade facade = (WebDriverFacade) getDriver();
-//        facade.withOptions("--headless");
+        facade.withOptions("--headless");
         return facade.getProxiedDriver();
     }
 
